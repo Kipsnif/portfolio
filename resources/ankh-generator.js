01833f2596419615ac8b55cff9584e8dfe65f95b
Matthijs <matthijshutten.drost@gmail.com>
2022-08-07 15:35:57 +0200

    Javascript idea

--- resources/ankh-generator.js
+++ resources/ankh-generator.js
@@ -1,4 +1,12 @@
 
+let ankhButton = document.getElementById('ankh-button');
+let ankh1 = document.getElementById('a1');
+let ankh2 = document.getElementById('a2');
+let ankh3 = document.getElementById('a3');
+let ankh4 = document.getElementById('a4');
+let ankh5 = document.getElementById('a5');
+
+
 const rgbGenerator = () => {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
@@ -7,9 +15,20 @@ const rgbGenerator = () => {
 }
 
 const visible = () => {
+    let x = Math.floor(Math.random() * 2);
+    if (x === 0) {
+        let y = 'visible'
+    } else {
+        let y = 'hidden'
+    }
+    return y
+}
+
+const generateAnkh = () => {
+    ankh1.target.style.color = rgbGenerator;
+    ankh1.target.style.visibility = visible;
 
 }
 
 
-
-
+ankhButton.onclick = generateAnkh
